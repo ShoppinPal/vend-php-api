fcc5238c195c92eb74528595b13445991a60d548
emul <fodor.laszlo@gmail.com>
2017-10-16 21:48:16 +0200

    Added new status

--- src/ShoppinPal/Vend/DataObject/Entity/V2/ConsignmentProduct.php
+++ src/ShoppinPal/Vend/DataObject/Entity/V2/ConsignmentProduct.php
@@ -6,9 +6,10 @@ use ShoppinPal\Vend\DataObject\Entity\EntityDoAbstract;
 
 class ConsignmentProduct extends EntityDoAbstract
 {
-    const STATUS_PENDING         = 'PENDING';
-    const STATUS_SENT_SUCCESS    = 'SENT_SUCCESS';
-    const STATUS_RECEIVE_SUCCESS = 'RECEIVE_SUCCESS';
+    const STATUS_PENDING           = 'PENDING';
+    const STATUS_SENT_SUCCESS      = 'SENT_SUCCESS';
+    const STATUS_RECEIVE_SUCCESS   = 'RECEIVE_SUCCESS';
+    const STATUS_STOCKTAKE_SUCCESS = 'STOCKTAKE_SUCCESS';
 
     public $productId;
     public $productSku;
